54b8b63f33dadd3b209b44568ad19ef427eab6cb
Diwa07 <khatiwadadiwakar07@gmail.com>
2025-09-02 23:05:25 +0545

    changing

--- src/app/Provider.js
+++
@@ -1,5 +0,0 @@
-import { Toaster } from "react-hot-toast";
-
-export default function Provider({ children }) {
-    return <><Toaster />{children}</>
-}
--- src/components/LoadingSpinner.jsx
+++
@@ -1,5 +0,0 @@
-export const LoadingSpinner = () => (
-    <div className="p-4 flex justify-center">
-        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-[#076E9E]"></div>
-    </div>
-);
